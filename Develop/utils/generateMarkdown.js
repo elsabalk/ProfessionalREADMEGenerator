cd0e722bee152cf5e96d2d65db3667afc1a011a6
Elsa Balkaran <elsabalk@hotmail.com>
2021-08-17 20:14:37 -0500

    Updated Table of Contents links

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -60,7 +60,7 @@ function generateMarkdown(data) {
   1. [Installation](#installation)
   2. [Usage](#usage)
   3. [License](#license)
-  4. [Contributing](#contributing)
+  4. [Contributing](#contributing) 
   5. [Tests](#tests)
   6. [Questions](#questions)
 
